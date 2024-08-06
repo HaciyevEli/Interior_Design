38ceba7f6d443997da9a7367a78c4c9f532d0d0c
HaciyevEli <haciyev.eli457@gmail.com>
2024-08-06 19:12:53 +0400

    complete header layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
 "use strict";
+$(document).ready(function(){
+  $(".header-silder").owlCarousel({
+    items: 1,
+    loop: true,
+    smartSpeed: 600,
+    margin: 5,
+  });
+
+  $('.silder-next').click(function() {
+    $(".header-silder").trigger('next.owl.carousel');
+  });
+  
+  $('.silder-prev').click(function() {
+    $(".header-silder").trigger('next.owl.carousel');
+  });
+});
+
+
+
