75a245fb6ad632a48fdb1a949772c1f3c23545c4
HaciyevEli <haciyev.eli457@gmail.com>
2024-08-09 00:46:44 +0400

    completed page development, fully responsive across all devices

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,33 @@
 "use strict";
-$(document).ready(function(){
+$(document).ready(function () {
+  const owlConfig = {
+    items: 1,
+    loop: true,
+    smartSpeed: 600,
+    margin: 15,
+    stagePadding: 40,
+    responsive:{
+      480:{
+        items: 2,
+        stagePadding: 30,
+      },
+      575: {
+        items: 3,
+        stagePadding: 0,
+      }
+    }
+  };
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth >= 768) {
+      $(".porfolio-content").trigger("destroy.owl.carousel");
+      $(".porfolio-content").classList.remove("owl-carousel");
+    } else {
+      $(".porfolio-content").owlCarousel(owlConfig);
+      $(".porfolio-content").classList.add("owl-carousel");
+    }
+  });
+
   $(".header-silder").owlCarousel({
     items: 1,
     loop: true,
@@ -7,15 +35,23 @@ $(document).ready(function(){
     margin: 5,
   });
 
-  $('.silder-next').click(function() {
-    $(".header-silder").trigger('next.owl.carousel');
-  });
-  
-  $('.silder-prev').click(function() {
-    $(".header-silder").trigger('next.owl.carousel');
+  $(".silder-next").click(function () {
+    $(".header-silder").trigger("next.owl.carousel");
   });
 
-  /****   FANCYBOX ****/ 
+  $(".silder-prev").click(function () {
+    $(".header-silder").trigger("next.owl.carousel");
+  });
+
+  // $(".porfolio-content-slider").owlCarousel({
+  //   items: 1,
+  //   loop: true,
+  //   smartSpeed: 600,
+  //   stagePadding: 40,
+  //   margin: 15,
+  // });
+
+  /****   FANCYBOX ****/
 
   Fancybox.bind("[data-fancybox]", {
     Thumbs: false,
@@ -28,6 +64,3 @@ $(document).ready(function(){
     },
   });
 });
-
-
-
